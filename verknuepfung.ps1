b4c2e95a023ea0ac3060f0405d8df5d564b2cd74
MarcusG <107219358+emge9x9@users.noreply.github.com>
2022-11-03 16:07:32 +0100

    Add files via upload

---
+++ verknuepfung.ps1
@@ -0,0 +1,4 @@
+﻿$WshShell = New-Object -comObject WScript.Shell
+$link = $wshshell.CreateShortcut(”$home\Desktop\Verknuepfung.lnk”)
+$link.targetpath = "C:\ProgramData\IBM\AMS4U\ams4u.ws"
+$link.save()
