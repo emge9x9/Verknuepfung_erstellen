f2a15e5cd282e27ed4ba3c209a34e0705a1cee08
MarcusG <107219358+emge9x9@users.noreply.github.com>
2022-11-03 16:12:45 +0100

    Update verknuepfung.ps1

--- verknuepfung.ps1
+++ verknuepfung.ps1
@@ -1,4 +1,6 @@
-﻿$WshShell = New-Object -comObject WScript.Shell
+$Datei = "C:\Program Files\..."
+
+$WshShell = New-Object -comObject WScript.Shell
 $link = $wshshell.CreateShortcut(”$home\Desktop\Verknuepfung.lnk”)
-$link.targetpath = "C:\ProgramData\IBM\AMS4U\ams4u.ws"
+$link.targetpath = $Datei
 $link.save()
